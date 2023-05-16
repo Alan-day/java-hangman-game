4e8ca8db790816d0d19076082a47eeae236565f5
Alan-day <lesterthenovice@gmail.com>
2023-05-16 14:49:52 +0100

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    //while loop (while lives != 0)
+    // input always in uppercase ( toUpper() )
+    // import java.util.Scanner;
+    // Scanner myObj = new Scanner(System.in)
+    // userInput = myObj.nextLine();
+    // class DisplayResults
+    // class storeUserInput
+    // class Game (Main)
+    // class random word selection
+    // transform word to underscore
+    // 	boolean find(userInput)
+    //  class random wordSelect
+    //  index[Random]
+}
---
+++ src/main/java/Words.java
@@ -0,0 +1,14 @@
+public class Words {
+
+    private String [] words = {"ELEPHANT", "PIGEON", "TIGER", "BEAR"};
+
+    public static void main(String[] args) {
+
+
+            public static getWord(){
+
+        }
+
+
+    }
+}
